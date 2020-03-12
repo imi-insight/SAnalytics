5eaa91913d45a6bbc660f124cbe72f70409f09e1
Souvik Samanta <souviksamanta95@gmail.com>
2020-03-12 16:43:19 +0530

    Create Souvik.R

---
+++ SAnalytics/R/Souvik.R
@@ -0,0 +1,54 @@
+# R Library Project - Bar chart
+
+# install.packages("ggplot2")
+library(ggplot2)
+
+head(iris)
+
+
+bar.freq <- function(x,y)
+{
+  #y <- as.character(k)
+  tt <- as.character(tit)
+  ggplot(x, aes(y))+
+    geom_bar(fill = "lightblue",color = "black")+
+    xlab(tt) +
+    ylab("Count")+
+    theme_classic()
+  print(tt)
+}
+
+bar.freq(iris,iris$Sepal.Length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
