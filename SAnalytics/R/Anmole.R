da81ab2f8b53087f407e624d6e763a4d691067a8
anmoledewan <anmoldew@gmail.com>
2020-03-13 03:39:05 +0530

    cleaned code for qplot with explanation

--- SAnalytics/R/Anmole.R
+++ SAnalytics/R/Anmole.R
@@ -1,49 +1,18 @@
-#' @title exports a histogram
-#'
-#' @description this function exports a histogram
-#'
-#' @param symbol
-#'
-#' @return NULL
-#'
-#' @examples  sgraph_histogram(iris$Sepal.Length,"Sepal Length","Histogram for sepal length")
-#'
-#' @export sgraph_histogram
-sgraph_histogram<-function(x,x.title,title)
+
+sgraph_histogram<-function(x,x.title,y.title="",title,
+                           fillcolor="blue",colcolor="white",
+                           alpha=I(1),binwidth=10)
 {
-  library(ggplot2)
-  qplot(x,
+
+  ggplot2::qplot(x,
         geom="histogram",
         main = title,
-        xlab = x.title,
-        fill=I("blue"),
-        col=I("red"),
-        alpha=I(.2))
-
-}
-
-#' @title exports a histogram with color options
-#'
-#' @description this function exports a histogram with color options
-#'
-#' @param symbol
-#'
-#' @return NULL
-#'
-#' @examples  sgraph_histogram(iris$Sepal.Length,"Sepal Length","Histogram for sepal length","red","blue")
-#'
-#' @export sgraph_histogram
-
-sgraph_histogram<-function(x,x.title,title,fillcolor,colcolor)
-{
-  library(ggplot2)
-  qplot(x,
-        geom="histogram",
-        main = title,
+        binwidth=binwidth,
+        ylab=y.title,
         xlab = x.title,
         fill=I(fillcolor),
         col=I(colcolor),
-        alpha=I(.2))
+        alpha=I(alpha))
 
 }
 
