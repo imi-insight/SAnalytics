b7f9b1d05d1f95b553bf09709c263ebd7a4917fb
Souvik Samanta <souviksamanta95@gmail.com>
2020-03-14 02:30:54 +0530

    Revert "Merge branch 'master' of https://github.com/imi-insight/SAnalytics"
    This reverts commit 12be0360d9795985d6de24c4545a68b017f0cb5e, reversing
    changes made to 944dec35d3457f514feed1bcaab79855a3d0457b.

--- SAnalytics/R/Anmole.R
+++ SAnalytics/R/Anmole.R
@@ -1,20 +1,49 @@
-
-sgraph_histogram<-function(x,x.title,y.title="",title,
-                           fillcolor="blue",colcolor="white",
-                           alpha=I(1),binwidth=10)
+#' @title exports a histogram
+#'
+#' @description this function exports a histogram
+#'
+#' @param symbol
+#'
+#' @return NULL
+#'
+#' @examples  sgraph_histogram(iris$Sepal.Length,"Sepal Length","Histogram for sepal length")
+#'
+#' @export sgraph_histogram
+sgraph_histogram<-function(x,x.title,title)
 {
-
-  ggplot2::qplot(x,
+  library(ggplot2)
+  qplot(x,
         geom="histogram",
         main = title,
-        binwidth=binwidth,
-        ylab=y.title,
         xlab = x.title,
-        fill=I(fillcolor),
-        col=I(colcolor),
-        alpha=I(alpha))
+        fill=I("blue"),
+        col=I("red"),
+        alpha=I(.2))
 
 }
 
+#' @title exports a histogram with color options
+#'
+#' @description this function exports a histogram
+#'
+#' @param symbol
+#'
+#' @return NULL
+#'
+#' @examples  sgraph_histogram(iris$Sepal.Length,"Sepal Length","Histogram for sepal length","red","blue")
+#'
+#' @export sgraph_histogram
 
-#devtools::document()
+sgraph_histogram<-function(x,x.title,title,fillcolor,colcolor)
+{
+  library(ggplot2)
+  qplot(x,
+        geom="histogram",
+        main = title,
+        xlab = x.title,
+        fill=I(fillcolor),
+        col=I(colcolor),
+        alpha=I(.2))
+
+}
+
