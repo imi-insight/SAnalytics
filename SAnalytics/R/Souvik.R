7cd3bd303789349c2925d8a96b2f3a88d2ef50e8
Souvik Samanta <souviksamanta95@gmail.com>
2020-03-14 13:48:31 +0530

    Update Souvik.R

--- SAnalytics/R/Souvik.R
+++ SAnalytics/R/Souvik.R
@@ -1,53 +1,11 @@
 # R Library Project - Bar chart
-
-# install.packages("ggplot2")
-
-
-head(iris)
-
-library(ggplot2)
-sgraph_bar <- function(data.main,data.col)
+sgraph_bar <- function(data.main,data.col,color="lightblue")
 {
-     #y <- as.character(k)
-     tt <- colnames(data.col)
-     ggplot(data.main, aes(data.col))+
-       geom_bar(fill = "lightblue",color = "black")+
-      # xlab(tt) +
-       ylab("Count")+
-       theme_classic()
+  y <- subset(data.main, select=data.col)
+  z <- as.vector(y[,1])
+  ggplot2::ggplot(data.main, aes(z))+
+    geom_bar(fill = color,color = "black")+
+    xlab(data.col) +
+    ylab("Count")+
+    theme_classic()
 }
-
-sgraph_bar(iris,iris$Sepal.Length)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
